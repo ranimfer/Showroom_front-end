87d1204c66263a0316b294d1bab030a755866b67
pokemon2780 <164089122+pokemon2780@users.noreply.github.com>
2025-04-12 07:12:34 +0100

    Update AdminDashboardPage.dart

--- lib/pages/AdminDashboardPage.dart
+++ lib/pages/AdminDashboardPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'ProductManagmentPage.dart';
+import 'UserManagmentPage.dart';
 
 class AdminDashboardPage extends StatelessWidget {
   const AdminDashboardPage({super.key});
@@ -7,15 +9,43 @@ class AdminDashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tableau de bord Admin'),
+        title: Text(
+          "Dashboard Admin",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[900] , 
       ),
-      body: const Center(
-        child: Text(
-          'Bienvenue sur le dashboard admin 👑',
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              child: Text("Menu Admin", style: TextStyle(color: Colors.white)),
+              decoration: BoxDecoration(color: Colors.blueGrey),
+            ),
+            ListTile(
+              title: const Text("Gestion Produits"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (_) => const ProductManagementPage(),
+                ));
+              },
+            ),
+            ListTile(
+              title: const Text("Gestion Utilisateurs"),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (_) => const UserManagementPage(),
+                ));
+              },
+            ),
+          ],
         ),
       ),
+      body: const Center(child: Text("Bienvenue sur le dashboard admin")),
     );
   }
 }
-
